fa3191178899c83af3929a5d5aab3a6bd7b52976
uenoyam <115358927+uenoyam@users.noreply.github.com>
2023-02-12 19:07:06 +0000

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   
   get 'followers/:id' => 'relationships#followers' , as: 'followers'
   get 'followings/:id' => 'relationships#followings' , as: 'followings'
+  get "search" => "searches#search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
